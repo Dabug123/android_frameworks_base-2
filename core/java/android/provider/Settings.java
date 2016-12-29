f619344fe919d483f04ec77d8fa3f9d6ccc7c03c
Marko Man <darkobas@gmail.com>
2016-12-29 01:07:25 +0100

    Configure lockscreen media artwork blur level
    Change-Id: I68895ae6a471bbc99b538b85337e15e8793665ce

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4658,6 +4658,12 @@ public final class Settings {
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
+         * Media artwork wallpaper blur level on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
