7fa05537d673a5c45d4a3160debc7c3f33474185
Lars Greiss <kufikugel@googlemail.com>
2018-10-27 16:14:57 +0530

    Add Music QS Tile
    Adapted to Pie by @KillerDroid96
    
    picked from here: http://goo.gl/ehaoMU
    
    Adapted to lollipop from commit:
    Author: kufikugel
    Quicksettings customizations Slim style
    
    Adapted double click to open music app from commit:
    Author: Jubakuba
    SystemUI: Pimp-up Media Tile (1/1)
    
    PS1: Gave proper credit to kufikugel for the original code
    PS2: Fix build by overriding all needed methods
    
    - Bring it to Oreo - LorDClockaN
    
    Change-Id: Iaab2514c3ff4c3b793fe579eb606fb5af4e258e2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -37,6 +37,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
@@ -110,6 +111,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CaffeineTile(mHost);
             case "immersive":
                 return new ImmersiveTile(mHost);
+            case "music":
+                return new MusicTile(mHost);
         }
 
         // Intent tiles.
