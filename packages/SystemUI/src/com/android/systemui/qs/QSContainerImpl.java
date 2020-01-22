85f91c2610d15fd3b4a86a3e7428d4a3ad62ebc8
Dabug123 <clarkbrunswick55@gmail.com>
2020-01-22 11:52:55 -0500

    Revert "QS: Fix opacity after reboot"
    This reverts commit 11b4f2b8967cfcbfc328f89c143541f694b3ef03.

--- packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -90,7 +90,6 @@ public class QSContainerImpl extends FrameLayout {
         super.onConfigurationChanged(newConfig);
         setBackgroundGradientVisibility(newConfig);
         updateResources();
-        updateSettings();
         mSizePoint.set(0, 0); // Will be retrieved on next measure pass.
     }
 
