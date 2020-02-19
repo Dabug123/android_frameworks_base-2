571614370fdc308c3ca35c89295a82191fa994c6
blinoff82 <blinoff82@users.noreply.github.com>
2020-02-19 17:08:08 +0100

    base: Add San Francisco Display Pro font [1/3]

--- core/java/com/android/internal/util/du/ThemesUtils.java
+++ core/java/com/android/internal/util/du/ThemesUtils.java
@@ -85,6 +85,7 @@ public class ThemesUtils {
             "com.android.theme.font.googlesans",
             "com.android.theme.font.oneplusslate",
             "com.android.theme.font.samsungone",
+            "com.android.theme.font.sanfrancisco",
             "com.android.theme.font.firasans",
             "com.android.theme.font.ubuntu",
     };
