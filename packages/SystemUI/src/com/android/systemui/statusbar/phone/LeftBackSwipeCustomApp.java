0a92a3d91bac523b849c81ebe34978a2044a4ed2
Dabug123 <clarkbrunswick55@gmail.com>
2020-12-05 22:26:56 +0100

    derp

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/LeftBackSwipeCustomApp.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/LeftBackSwipeCustomApp.java
@@ -6,8 +6,8 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.ListView;
 
-import com.abc.support.R;
-import com.abc.support.preferences.AppPicker;
+import com.dirtyunicorns.support.R;
+import com.dirtyunicorns.support.preferences.AppPicker;
 
 public class LeftBackSwipeCustomApp extends AppPicker {
 
